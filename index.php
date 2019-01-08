4bca9ba0c52cabb8faba7dc4af2f8a9315f43b1f
nadeeshapathiraja <pdncpathiraja95@gmail.com>
2019-01-09 01:06:09 +0530

    make log out

--- index.php
+++ index.php
@@ -12,6 +12,7 @@
         <li><a href="displayNews.php">News</a></li>	
         <li><a href="aboutUs.php">About Us</a></li>
         <li><a href="contact.php">Contacts</a></li>
+        <li><a class="logOut" href="loginForm.php">Log Out</a></li>
 
 	</ul>
   
